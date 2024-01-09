bdad3229d0fa43996dbe3a2c3d2827e630c3de93
sulumor <romuald.patfoort@gmail.com>
2024-01-09 17:01:24 +0100

    fix: forgot next in validate Mw

--- app/validations/validate.middleware.js
+++ app/validations/validate.middleware.js
@@ -3,6 +3,7 @@ import ApiError from "../errors/api.error.js";
 export default (dataProp, schema) => async (req, res, next) => {
   try {
     await schema.validateAsync(req[dataProp]);
+    next();
   } catch (err) {
     next(new ApiError(err.details[0].message, { httpStatus: 400 }));
   }
