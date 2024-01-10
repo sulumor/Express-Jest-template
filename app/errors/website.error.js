5f82c51ff14525123e0a76511189cc89f9e23866
sulumor <romuald.patfoort@gmail.com>
2024-01-10 22:56:59 +0100

    feat: add websiteError class

--- app/errors/website.error.js
+++ app/errors/website.error.js
@@ -1,23 +1,14 @@
-/**
- * @typedef {object} ApiJsonErro - Error response
- * @property {string} error.required - Error message
- * @example
- * {
- *  "error": "Bad request"
- * }
- */
-
-export default class ApiError extends Error {
+export default class WebsiteError extends Error {
   /**
    * Personnalizes Error Constructor to personnalized user response
    * @param {string} message contains literal message to communicate
    * @param {object} info dynamic object to add more informations to the error (ex: http status)
    */
-  constructor(message, infos) {
+  constructor(message, info) {
     super(message);
-    this.name = "Api error";
-    this.format = "json";
-    this.httpStatus = infos.httpStatus || 500;
+    this.name = "WebsiteError";
+    this.format = "html";
+    this.httpsStatus = info.httpsStatus || 500;
     // * A décommenter pour ajouter plus de paramètres
     // Object.entries(infos).forEach(([key, value]) => {
     //   this[key] = value;
