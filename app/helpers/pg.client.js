b303dd5e3ddbf04f94ea7a39dceb1c112c5c4270
Romuald Patfoort <74116635+sulumor@users.noreply.github.com>
2024-02-12 09:16:37 +0100

    Update pg.client.js

--- app/helpers/pg.client.js
+++ app/helpers/pg.client.js
@@ -2,7 +2,7 @@ import pg from "pg";
 
 const client = new pg.Pool({
   host: process.env.PGHOST,
-  user: process.env.PHUSER,
+  user: process.env.PGUSER,
   database: process.env.PGDATABASE,
 });
 
